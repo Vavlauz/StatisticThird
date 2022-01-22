4e9bc54f45b83e9f0f0ec4c2dcfae3eb9d765980
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-01-22 19:59:51 +0700

    Initial commit

---
+++ src/test/java/ru/netology/statistic_third/StatisticServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.statistic_third;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxSecond() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 25, 11, 12};
+        long expected = 25;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+}
